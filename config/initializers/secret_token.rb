23a04dbc348e4f65a5ae43b7396f346686404f12
oldtimer75 <dszeto@gmail.com>
2014-04-29 06:10:26 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fovis::Application.config.secret_key_base = '35eec0be47a671895f18e12b6e752610e913990508cbf6f384b018b205b046073e11bdecfb2e7fe8c78aba876432edd91643c932b3f5d4a9d998753a86ba6e0a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fovis::Application.config.session_store :cookie_store, key: '_fovis_session'
