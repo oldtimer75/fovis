114b6680726ccb8a247a1ac374d2f9f8372adf6d
oldtimer75 <dszeto@gmail.com>
2014-04-29 07:09:04 +0000

    created the candidates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Fovis::Application.routes.draw do
-  
-  root to: "home#index" 
+  resources :candidates
+  root to: "candidates#index" 
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
